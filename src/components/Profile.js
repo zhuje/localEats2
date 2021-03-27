481f18d4a3b277feba71e18c5e0db560e802b683
LoganMednick <mednick.l@husky.neu.edu>
2021-03-27 13:34:47 -0400

    Started promotions and profile designs.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {HorizontalBar} from 'react-chartjs-2';
+import {Rectangle, Circle, Ellipse, Line, Polyline, CornerBox, Triangle} from 'react-shapes';
 import "../style/style.profile.css"
 
 
@@ -8,6 +10,17 @@ export default class Profile extends React.Component{
             <div>
                 <div className="col-sm-12 col-md-9">
                     <h1> Profile </h1>
+                    <Rectangle width={500} height={100} fill={{color:'#2409ba'}} stroke={{color:'#E65243'}} strokeWidth={3} />
+                    <div className={"dev_transbox"} >
+                        <form>
+                            <input type="checkbox" id="allston" name="allston" value="allston"/>
+                            <label htmlFor="allston"> &nbsp; Allston </label> <br/>
+                            <input type="checkbox" id="backbay" name="backbay" value="backbay"/>
+                            <label htmlFor="backbay"> &nbsp; Back Bay </label> <br/>
+                            <input type="checkbox" id="jamaica" name="jamaica" value="jamaica"/>
+                            <label htmlFor="jamaica"> &nbsp; Jamaica Plain </label> <br/>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
--- src/components/Promotions.js
+++ src/components/Promotions.js
@@ -1,13 +1,44 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import "../style/style.promotions.css"
 
-
-export default class Promotions extends React.Component{
-    render(){
-        return(
+export default class Promotions extends React.Component {
+    render() {
+        return (
             <div>
                 <div className="col-sm-12 col-md-9">
-                    <h1> Promotions </h1>
+                    <h1>Promotions</h1>
+                    <div className={"container"}>
+                        <div className={"row"}>
+                            <Card style={{width: '18rem'}}>
+                                <Card.Img variant="top"
+                                          src="https://northeastern.instructure.com/courses/63357/files/7382149/download"/>
+                                <Card.Body>
+                                    <Card.Title>Tres Gatos</Card.Title>
+                                    <Card.Text>
+                                        $5 Margaritas - March 30th to April 5th
+                                    </Card.Text>
+                                    <Button variant="primary">Add</Button>
+                                    <Button variant="secondary">Remove</Button>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{width: '18rem'}}>
+                                <Card.Img variant="top"
+                                          src="https://northeastern.instructure.com/courses/63357/files/7382149/download"/>
+                                <Card.Body>
+                                    <Card.Title>Zesto's Pizza</Card.Title>
+                                    <Card.Text>
+                                        Buy one large pizza, get one half off! Offer good until
+                                        4/1/21.
+                                    </Card.Text>
+                                    <Button variant="primary">Add</Button>
+
+                                    <Button variant="secondary">Remove</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
