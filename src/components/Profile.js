b26e94429ef72d81793c280c60e955bb99b26c80
LoganMednick <mednick.l@husky.neu.edu>
2021-03-30 00:58:51 -0400

    Fixed a lot lol

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,80 @@
 import React from "react";
-import {HorizontalBar} from 'react-chartjs-2';
-import {Rectangle, Circle, Ellipse, Line, Polyline, CornerBox, Triangle} from 'react-shapes';
 import "../style/style.profile.css"
+import Button from "react-bootstrap/Button";
 
+export default class Profile extends React.Component {
+    render() {
+        return (
+            <div className="col-sm-12 col-md-9">
+                <div className="dev_background">
+                    <div className="dev_transbox">
+                        <h1 className="dev_title"> Set up your profile </h1>
+                        <p> Use this window to select your preferences and save them to your
+                            profile.
+                            This helps us recommend eatery choices tailored to your interests!
+                        </p>
+                        <div className={"rowC"}>
+                            <div className="col">
+                                <h2 className="dev_title"> Preferred Neighborhoods: </h2>
+                                <form>
+                                    <input type="checkbox" id="allston" name="allston"
+                                           value="allston"/>
+                                    <label htmlFor="allston"> &nbsp; Allston </label> <br/>
+                                    <input type="checkbox" id="backbay" name="backbay"
+                                           value="backbay"/>
+                                    <label htmlFor="backbay"> &nbsp; Back Bay </label> <br/>
+                                    <input type="checkbox" id="jamaica" name="jamaica"
+                                           value="jamaica"/>
+                                    <label htmlFor="jamaica"> &nbsp; Jamaica Plain </label> <br/>
+                                </form>
 
-export default class Profile extends React.Component{
-    render(){
-        return(
-            <div>
-                <div className="col-sm-12 col-md-9">
-                    <h1> Profile </h1>
-                    <Rectangle width={500} height={100} fill={{color:'#2409ba'}} stroke={{color:'#E65243'}} strokeWidth={3} />
-                    <div className={"dev_transbox"} >
-                        <form>
-                            <input type="checkbox" id="allston" name="allston" value="allston"/>
-                            <label htmlFor="allston"> &nbsp; Allston </label> <br/>
-                            <input type="checkbox" id="backbay" name="backbay" value="backbay"/>
-                            <label htmlFor="backbay"> &nbsp; Back Bay </label> <br/>
-                            <input type="checkbox" id="jamaica" name="jamaica" value="jamaica"/>
-                            <label htmlFor="jamaica"> &nbsp; Jamaica Plain </label> <br/>
-                        </form>
+                                <h2 className="dev_title"> Preferred Price Range: </h2>
+                                <form>
+                                    <input type="checkbox" id="allston" name="allston"
+                                           value="allston"/>
+                                    <label htmlFor="allston"> &nbsp; $ </label> <br/>
+                                    <input type="checkbox" id="backbay" name="backbay"
+                                           value="backbay"/>
+                                    <label htmlFor="backbay"> &nbsp; $$ </label> <br/>
+                                    <input type="checkbox" id="jamaica" name="jamaica"
+                                           value="jamaica"/>
+                                    <label htmlFor="jamaica"> &nbsp; $$$ </label> <br/>
+                                </form>
+
+                                <Button variant="primary">Submit</Button>
+
+                            </div>
+                            <div className="col">
+                                <h2 className="dev_title"> Preferred Neighborhoods: </h2>
+                                <form>
+                                    <input type="checkbox" id="allston" name="allston"
+                                           value="allston"/>
+                                    <label htmlFor="allston"> &nbsp; Allston </label> <br/>
+                                    <input type="checkbox" id="backbay" name="backbay"
+                                           value="backbay"/>
+                                    <label htmlFor="backbay"> &nbsp; Back Bay </label> <br/>
+                                    <input type="checkbox" id="jamaica" name="jamaica"
+                                           value="jamaica"/>
+                                    <label htmlFor="jamaica"> &nbsp; Jamaica Plain </label> <br/>
+                                </form>
+
+                                <h2 className="dev_title"> Preferred Price Range: </h2>
+                                <form>
+                                    <input type="checkbox" id="allston" name="allston"
+                                           value="allston"/>
+                                    <label htmlFor="allston"> &nbsp; $ </label> <br/>
+                                    <input type="checkbox" id="backbay" name="backbay"
+                                           value="backbay"/>
+                                    <label htmlFor="backbay"> &nbsp; $$ </label> <br/>
+                                    <input type="checkbox" id="jamaica" name="jamaica"
+                                           value="jamaica"/>
+                                    <label htmlFor="jamaica"> &nbsp; $$$ </label> <br/>
+                                </form>
+
+                                <Button variant="primary">Submit</Button>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Promotions.js
+++ src/components/Promotions.js
@@ -1,46 +1,81 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import "../style/style.promotions.css"
 
 export default class Promotions extends React.Component {
     render() {
         return (
-            <div>
                 <div className="col-sm-12 col-md-9">
                     <h1>Promotions</h1>
-                    <div className={"container"}>
-                        <div className={"row"}>
-                            <Card style={{width: '18rem'}}>
-                                <Card.Img variant="top"
-                                          src="https://northeastern.instructure.com/courses/63357/files/7382149/download"/>
-                                <Card.Body>
-                                    <Card.Title>Tres Gatos</Card.Title>
-                                    <Card.Text>
-                                        $5 Margaritas - March 30th to April 5th
-                                    </Card.Text>
-                                    <Button variant="primary">Add</Button>
-                                    <Button variant="secondary">Remove</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{width: '18rem'}}>
-                                <Card.Img variant="top"
-                                          src="https://northeastern.instructure.com/courses/63357/files/7382149/download"/>
-                                <Card.Body>
-                                    <Card.Title>Zesto's Pizza</Card.Title>
-                                    <Card.Text>
-                                        Buy one large pizza, get one half off! Offer good until
-                                        4/1/21.
-                                    </Card.Text>
-                                    <Button variant="primary">Add</Button>
+                    <div class="card-columns">
+                        <Card style={{width: '20rem'}}>
+                            <Card.Img variant="top"
+                                      src="https://www.jamaicaplainnews.com/wp-content/uploads/2015/08/Tres-Gatos1.jpg"
+                                      className="promoCard"
+                            />
+                            <Card.Body>
+                                <Card.Title>Tres Gatos</Card.Title>
+                                <Card.Text>
+                                    $5 Margaritas all day! - March 30th to April 5th. Come on down!
+                                </Card.Text>
+                                <Button variant="primary">Add</Button>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <Button variant="secondary">Remove</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{width: '20rem'}}>
+                            <Card.Img variant="top"
+                                      src="https://i1.ypcdn.com/blob/127d71a9db0a06311aa67618e69f6375a2ba5a1b_400x280_crop.jpg"
+                                      className="promoCard"
+                            />
+                            <Card.Body>
+                                <Card.Title>Zesto's Pizza</Card.Title>
+                                <Card.Text>
+                                    Buy one large pizza, get one half off! Offer good until
+                                    4/1/21.
+                                </Card.Text>
+                                <Button variant="primary">Add</Button>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <Button variant="secondary">Remove</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{width: '20rem'}}>
+                            <Card.Img variant="top"
+                                      src="https://api.curaytor.io/sites/20/media/419380/CanaryExt.jpg?w=851"
+                                      className="promoCard"
+                            />
+                            <Card.Body>
+                                <Card.Title>Canary Square</Card.Title>
+                                <Card.Text>
+                                    A really great promotion! It will be great once I figure out
+                                    what it is.
+                                </Card.Text>
+                                <Button variant="primary">Add</Button>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <Button variant="secondary">Remove</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{width: '20rem'}}>
+                            <Card.Img variant="top"
+                                      src="https://elorientaldecuba.net/100_0455.JPG"
+                                      className="promoCard"
+                            />
 
-                                    <Button variant="secondary">Remove</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
+                            <Card.Body>
+                                <Card.Title>El Oriental De Cuba</Card.Title>
+                                <Card.Text>
+                                    There will be another great promotion put here I promise!!
+                                </Card.Text>
+                                <Button variant="primary">Add</Button>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <Button variant="secondary">Remove</Button>
+                            </Card.Body>
+                        </Card>
                     </div>
                 </div>
-            </div>
         )
     }
 }
