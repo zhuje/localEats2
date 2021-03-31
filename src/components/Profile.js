a861d248f675f375e51789fbf3efecafc8024ba2
LoganMednick <mednick.l@husky.neu.edu>
2021-03-31 17:27:07 -0400

    added popups, changed profile layout, added promotion sort tabs

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../style/style.profile.css"
 import Button from "react-bootstrap/Button";
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+import Card from "react-bootstrap/Card";
 
 export default class Profile extends React.Component {
     render() {
@@ -40,40 +43,68 @@ export default class Profile extends React.Component {
                                            value="jamaica"/>
                                     <label htmlFor="jamaica"> &nbsp; $$$ </label> <br/>
                                 </form>
-
-                                <Button variant="primary">Submit</Button>
-
                             </div>
                             <div className="col">
                                 <h2 className="dev_title"> Preferred Restaurants: </h2>
                                 <form>
-                                    <input type="checkbox" id="allston" name="allston"
-                                           value="allston"/>
-                                    <label htmlFor="allston"> &nbsp; American </label> <br/>
-                                    <input type="checkbox" id="backbay" name="backbay"
-                                           value="backbay"/>
-                                    <label htmlFor="backbay"> &nbsp; Asian </label> <br/>
-                                    <input type="checkbox" id="jamaica" name="jamaica"
-                                           value="jamaica"/>
-                                    <label htmlFor="jamaica"> &nbsp; Garbage </label> <br/>
+                                    <input type="checkbox" id="american" name="american"
+                                           value="american"/>
+                                    <label htmlFor="american"> &nbsp; American </label> <br/>
+                                    <input type="checkbox" id="chinese" name="chinese"
+                                           value="chinese"/>
+                                    <label htmlFor="chinese"> &nbsp; Chinese </label> <br/>
+                                    <input type="checkbox" id="indian" name="indian"
+                                           value="indian"/>
+                                    <label htmlFor="indian"> &nbsp; Indian </label> <br/>
+                                    <input type="checkbox" id="italian" name="italian"
+                                           value="italian"/>
+                                    <label htmlFor="italian"> &nbsp; Italian </label> <br/>
+                                    <input type="checkbox" id="mexican" name="mexican"
+                                           value="mexican"/>
+                                    <label htmlFor="italian"> &nbsp; Mexican </label> <br/>
+                                    <input type="checkbox" id="cafe" name="cafe"
+                                           value="cafe"/>
+                                    <label htmlFor="cafe"> &nbsp; Café </label> <br/>
+                                    <input type="checkbox" id="fastfood" name="fastfood"
+                                           value="fastfood"/>
+                                    <label htmlFor="fastfood"> &nbsp; Pub </label> <br/>
+                                    <input type="checkbox" id="veg" name="veg"
+                                           value="veg"/>
+                                    <label htmlFor="veg"> &nbsp; Vegan / Vegetarian </label> <br/>
                                 </form>
-
-                                <h2 className="dev_title"> Preferred Price Range: </h2>
-                                <form>
-                                    <input type="checkbox" id="allston" name="allston"
-                                           value="allston"/>
-                                    <label htmlFor="allston"> &nbsp; $ </label> <br/>
-                                    <input type="checkbox" id="backbay" name="backbay"
-                                           value="backbay"/>
-                                    <label htmlFor="backbay"> &nbsp; $$ </label> <br/>
-                                    <input type="checkbox" id="jamaica" name="jamaica"
-                                           value="jamaica"/>
-                                    <label htmlFor="jamaica"> &nbsp; $$$ </label> <br/>
-                                </form>
-
-                                <Button variant="primary">Submit</Button>
 
                             </div>
+                        </div>
+                        <div className="submit">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;
+                            <Popup trigger={<Button variant="primary"> Submit </Button>}
+                                   position="left center">
+                                <div> This button will save the user's preferences in future
+                                    implementations.
+                                </div>
+                            </Popup>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;
+                            <Popup trigger={<Button variant="danger"> Reset </Button>}
+                                   position="top">
+                                <div> This button will reset the user's preferences back to the
+                                    default
+                                    settings, which would erase each of their selections.
+                                </div>
+                            </Popup>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;
+                            <Popup trigger={<Button variant="info"> Saved Promotions </Button>}
+                                   position="right center">
+                                <div> This button will allow the user to check out each promotion
+                                    they
+                                    have saved to their profile from the promotions tab.
+                                </div>
+                            </Popup>
                         </div>
                     </div>
                 </div>
