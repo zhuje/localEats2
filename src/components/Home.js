1bb31baeacf5201427ebbd26a9127a533e092198
zhuje <59589720+zhuje@users.noreply.github.com>
2021-04-16 17:39:17 -0400

    update filtering

--- src/components/Home.js
+++ src/components/Home.js
@@ -60,39 +60,10 @@ export default class Home extends React.Component {
                     <div className="dev_background">
                         <div className="dev_transbox">
 
-                            {/*<Appy />*/}
-                            {/* /!*Button trigger modal *!/*/}
-                            {/*<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">*/}
-                            {/*    Launch demo modal*/}
-                            {/*</button>*/}
-
-                            {/*/!* Modal *!/*/}
-                            {/*<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">*/}
-                            {/*    <div class="modal-dialog" role="document">*/}
-                            {/*        <div class="modal-content">*/}
-                            {/*            <div class="modal-header">*/}
-                            {/*                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>*/}
-                            {/*                <button type="button" class="close" data-dismiss="modal" aria-label="Close">*/}
-                            {/*                    <span aria-hidden="true">&times;</span>*/}
-                            {/*                </button>*/}
-                            {/*            </div>*/}
-                            {/*            <div class="modal-body">*/}
-                            {/*                ...*/}
-                            {/*            </div>*/}
-                            {/*            <div class="modal-footer">*/}
-                            {/*                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>*/}
-                            {/*                <button type="button" class="btn btn-primary">Save changes</button>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
-
-
-
-
                             {/* Welcome */}
-                            <h1 className="dev_title_jumbo dev_title"> Welcome to Local Eats </h1>
-                            <h3 className="dev_title" > Helping You Find Locally Owned Eateries in Boston </h3>
+                            <h1 className="dev_title_jumbo dev_title"> Welcome to LocalEats </h1>
+                            <h3 className="dev_title"> Helping You Find Locally Owned Eateries in Boston </h3>
+
                             <hr className="my-4"/>
 
 
